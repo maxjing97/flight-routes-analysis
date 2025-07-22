a7cc5a64e9cc067e65d07cb6a63606c8156fa648
Max Jing <maxjing97@gmail.com>
2025-07-21 23:57:15 -0500

    fixed caption.

--- src/pages/route_explorer.js
+++ src/pages/route_explorer.js
@@ -36,25 +36,26 @@ export function Entry() { //entry function allowing more information to be found
 export function Trends() {
   return (
     <div className="about_main" style={styles.about_main}> 
-      <h1 className='stats-title'>About</h1>
-      <h3>Start becoming proficient in reading Mandarin!</h3>
-      <h2>practice and learn now!</h2>
-
-      <p>This is a project to create a flashcard app that also assigns in learning chinese by having characters ranked in a frequency order</p>
-      <p>This tool is likely most helpful to people wanting to practice their character and mandarin pronunciation comprehension who are at an intermediate to early advanced
-       level of chinese. The character list contains ~2600 to practice, and the dictionary contains around ~10000 total words that are are combination of these characters.
-    
-      
-      The original ranking is based on a <a href="http://www.mementoslangues.fr/Chinois/Sinogrammes/Table3000CaracteresChinois.pdf">linked pdf </a>
-      here. 
-
-      Edits were made with additional resources <br></br><br></br>
-
-      This is an open source project with the <a href="https://github.com/maxjing97/mandarin_chinese_characters_test">github link</a> here.<br></br>
-      
-      Contact maxjing97@gmail for support or questions. 
-      </p>
-
+      <h1 className='stats-title'>Wikipedia Flights Data Explorer</h1>
+      <h3>Explore wikipedia flight routes data and COVID-19 trends from wikipedia data</h3>
+      <div className="essay">
+        <p>
+          Explore webscrapped route data (data current as of July 19th 2025) for the top 959 airports in the world. 
+        </p>
+        <p>
+        Also, compare trends in the number of routes per airport according to wikipedia data from the top 979 airports. View changes in routes for these airports by three year ranges: <br></br> 
+        </p>
+          <ul>
+            <li>end of 2021 (pre2022) to current</li>
+            <li>end of 2019 (pre2020) to current</li>
+            <li>end of 2019 to end of 2021</li>
+          </ul>
+        <p>Note that since wikipedia relies on user contribution, much of the data on routes, especially at the smaller airports on the list, 
+        is likely to be not be too accurate, especially immediately after the start of the COVID-19 pandemic in 2020.</p>
+        <p>More information about how data was collected is found in flight trends </p>
+        <p>A powerBI file with detailed analysis is linked for download here: <a href="./routes.analysis.pbix" download="analysis.pbix">Download File</a>
+        </p>
+      </div>
     </div>
   );
 }
