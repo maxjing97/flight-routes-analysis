80ad1713a5b5eaa75807270f30e116aadf023459
Max Jing <maxjing97@gmail.com>
2025-07-23 16:59:41 -0500

    implemented a BFS algorithm for connections.

--- src/pages/route_explorer.js
+++ src/pages/route_explorer.js
@@ -17,11 +17,73 @@ const getIdx = (iata) => {
   return -1
 }
 
+//function to get the possible airlines between two destination iata (needed since BFS/A* algorithm) matches based on iata/first
+const getAirlines = (source, dest) => {
+  const found_airlines = [] //list of airlines found 
+  //get connections list 
+  const connections = network_graph[source]
+  for(const node of connections) {
+    if (node[1] === dest) {
+      found_airlines.push(node[2])
+    }
+  }
+  return found_airlines
+}
+//from a previous array from the BFS or A* function, get the path in ascending order from a source to a destination
+const getPath = (source, dest, prev) => {
+  let path = [] //store the path. Add from the start by using unshift instead
+  let current_iata = dest//get the current next iata from the prev, intiialze with the current one
+  path.unshift(current_iata)
+  while (current_iata !== source) {
+    current_iata = prev[current_iata]
+    path.unshift(current_iata)
+  }
+  return path
+}
+
+//bfs algorithm with source iata and destination iata 
+const BFS = (iata, dest_iata) => {
+  //mark all iata other destinations as unvisited (infinite distance from the start)
+  let visited = {} 
+  let prev = {} //previous array of the components
+  for(const key in network_graph) {
+    visited[key] = Infinity //set to 
+    prev[key] = null //set to 
+  }
+  visited[iata] = 0//set the current visited node distane to be 0
+  prev[iata] = iata //previous for itself is clearly this 
+  const Q = [iata] //use a list as queue
+  while (Q.length > 0) { //while queue is not empty
+    const curriata = Q.shift() //remove starting element
+    //get neighbors list of tuples of [distance, iata, airline]
+    const neighbors = network_graph[curriata]   
+    for (const neighbor of neighbors) {
+      const neighbor_iata = neighbor[1]//get the iata of the neighbor
+      if (visited[neighbor_iata]===Infinity) { //only added to next visited
+        visited[neighbor_iata] = visited[curriata] + 1 //add 1 for the distance
+        Q.push(neighbor_iata)//add to the queue 
+        prev[neighbor_iata] = curriata//store the previous 
+        //this part is excluded in standard bfs if we want to find the shortest number of connections to a certain node, we stop here
+        if (dest_iata === neighbor_iata) {
+          return [visited[neighbor_iata], prev] //return the distance and the previous map
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
 function SearchAirports({initialIata = "LHR", setResult=()=>{}}) {  
   const initial_index = getIdx(initialIata)
   const [inputText, setInputText] = useState(`${network_airports[initial_index]["IATA"]}`) ///initial text that appears in the input box 
   const [searchData, setSearchData] = useState([]) //this is the state storing current search data options to the user
-  
 
   const handleInput = (e)=> {
     const text = e.target.value.toLowerCase().trim() 
@@ -72,11 +134,15 @@ function SearchAirports({initialIata = "LHR", setResult=()=>{}}) {
   );
 }
 
-
+ 
 export function RouteFinder() { //entry function allowing more information to be found out
-  const [sourceData, setSourceData] = useState(network_airports[getIdx("LHR")]) //store the source airport data
-  const [destData, setDestData] = useState(network_airports[getIdx("NRT")]) //store the dest airport data
-
+  const [sourceData, setSourceData] = useState(network_airports[getIdx("LHR")]) //store the source airport data (default origin)
+  const [destData, setDestData] = useState(network_airports[getIdx("NRT")]) //store the dest airport data (default destination)
+  const [BFSresults, setBFSresults] = useState(BFS("LHR", "NRT"))//store bfs results
+  //store results
+  useEffect(()=>{
+    setBFSresults(BFS(sourceData["IATA"], destData["IATA"]))
+  }, [sourceData, destData]) //change the BFS result if the 
 
   return (
     <div className="about_main"> 
@@ -88,6 +154,32 @@ export function RouteFinder() { //entry function allowing more information to be
       </div>
       <h3>Finding the shortest routes </h3>
       <h3>From {sourceData["wiki_name"].replaceAll("_"," ")} ({sourceData["IATA"]}) to {destData["wiki_name"].replaceAll("_"," ")} ({destData["IATA"]})</h3>
+
+      {BFSresults[0]===1 && 
+        <div>
+          <h3>There's a direct flight operated by the following airlines:</h3>
+          {getAirlines(sourceData["IATA"], destData["IATA"]).map((airline, index) => (
+            <p>{airline}</p>
+          ))}
+        </div>
+      }
+      
+      {BFSresults[0]!==1 && 
+        <div>
+          <h3>There's no direct flight. Here's a route with the fewest connections ({BFSresults[0]})</h3>
+          {/*retrace the path using the previous dict*/}
+          <h3>Flight path:</h3>
+          <div className="paths">
+            
+            {
+              getPath(sourceData["IATA"], destData["IATA"], BFSresults[1]).map((airport, index) => (
+                <h3>{`${airport}=>`}</h3>
+              ))
+            }
+          </div>
+        </div>
+      }
+    
     </div>
   );
 }
